aa4658b529b5177436b629808d5b78111a1bb1fb
eyosi <145581542+E7777778@users.noreply.github.com>
2025-04-10 19:51:33 +0300

    DigitalMarketngAgency

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+'use strict';
+const addEventonElement =function(elements,eventType,callback){
+    for(let i=0,len=elements.length;i<len;i++){
+        elements[i].addEventListener(eventType,callback);
+
+    }
+}
+
+const navbar=document.querySelector("[data-navbar]");
+const navToggleBtn=document.querySelector("[data-nav-toggle-btn]");
+const overlay=document.querySelector("[data-overla]");
+const toggleNavbar=function(){
+    navbar.classList.toggle("active");
+    navToggleBtn.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+
+
+}
+addEventonElement([navToggleBtn,overlay],"click",toggleNavbar);
